d23edd85567940d44647c9bda8be143556daa81b
Kyle Malinowski <kmalino66@gmail.com>
2017-11-12 00:08:33 -0600

    Initial Commit. Created project.

---
+++ app/src/main/java/net/trizmo/riderradar/MainActivity.java
@@ -0,0 +1,13 @@
+package net.trizmo.riderradar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

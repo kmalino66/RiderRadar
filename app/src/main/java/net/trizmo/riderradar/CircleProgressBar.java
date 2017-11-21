bd009a787c765c16722d825c6cfc5aeab2e2c458
Kyle Malinowski <kmalino66@gmail.com>
2017-11-21 12:31:12 -0600

    Create the CircularProgressBar, and designed the Main Activity.

---
+++ app/src/main/java/net/trizmo/riderradar/CircleProgressBar.java
@@ -0,0 +1,108 @@
+package net.trizmo.riderradar;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by trizm on 11/20/2017.
+ */
+
+public class CircleProgressBar extends View {
+
+
+    /**
+     * ProgressBar's line thickness
+     */
+    private float strokeWidth = 4;
+    private float progress = 0;
+    private int min = 0;
+    private int max = 100;
+    /**
+     * Start the progress at 12 o'clock
+     */
+    private int startAngle = -90;
+    private int color = Color.DKGRAY;
+    private int backgroundColor = Color.parseColor("#90a4ae");
+    private RectF rectF;
+    private Paint backgroundPaint;
+    private Paint foregroundPaint;
+    private Paint textPaint;
+
+    public CircleProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        rectF = new RectF();
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(
+                 attrs,
+                R.styleable.CircleProgressBar,
+                0, 0);
+        //Reading values from the XML layout
+        try {
+            strokeWidth = typedArray.getDimension(R.styleable.CircleProgressBar_progressBarThickness, strokeWidth);
+            progress = typedArray.getFloat(R.styleable.CircleProgressBar_progress, progress);
+            color = typedArray.getInt(R.styleable.CircleProgressBar_progressbarColor, color);
+            min = typedArray.getInt(R.styleable.CircleProgressBar_min, min);
+            max = typedArray.getInt(R.styleable.CircleProgressBar_max, max);
+        } finally {
+            typedArray.recycle();
+        }
+
+        backgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        backgroundPaint.setColor(backgroundColor);
+        backgroundPaint.setStyle(Paint.Style.STROKE);
+        backgroundPaint.setStrokeWidth(strokeWidth / 2);
+
+        foregroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        foregroundPaint.setColor(color);
+        foregroundPaint.setStyle(Paint.Style.STROKE);
+        foregroundPaint.setStrokeWidth(strokeWidth);
+
+        textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textPaint.setColor(backgroundColor);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        textPaint.setTypeface(Typeface.DEFAULT);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        final int height = getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+        final int width = getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+        final int min = Math.min(width, height);
+        setMeasuredDimension(min, min);
+        rectF.set(0 + strokeWidth / 2, 0 + strokeWidth / 2, min - strokeWidth / 2, min - strokeWidth / 2);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        textPaint.setTextSize(canvas.getHeight() / 2);
+        canvas.drawOval(rectF, backgroundPaint);
+        float angle = 360 * progress / max;
+        canvas.drawArc(rectF, startAngle, angle, false, foregroundPaint);
+
+        Rect bounds = new Rect();
+        textPaint.getTextBounds((int)(progress) + "", 0, ((int)(progress) + "").length(), bounds);
+        int height = bounds.height();
+
+        canvas.drawText((int)(progress) + "", canvas.getWidth() / 2, (canvas.getHeight() / 2) + (height / 2), textPaint);
+
+
+    }
+
+    public void setProgress(float progress) {
+        this.progress = progress;
+        invalidate();// Notify the view to redraw it self (the onDraw method is called)
+    }
+}

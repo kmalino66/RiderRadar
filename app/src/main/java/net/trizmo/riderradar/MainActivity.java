497910b71e83b00efe086688bae391f43b0eb5ce
Kyle Malinowski <kmalino66@gmail.com>
2017-11-22 17:31:17 -0600

    Added in some structural and outline code for Main Activity

--- app/src/main/java/net/trizmo/riderradar/MainActivity.java
+++ app/src/main/java/net/trizmo/riderradar/MainActivity.java
@@ -2,6 +2,9 @@ package net.trizmo.riderradar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setProgressBarClickListener();
+    }
+
+    private void setProgressBarClickListener()
+    {
+        CircleProgressBar circleProgressBar = (CircleProgressBar) findViewById(R.id.custom_progressBar);
+        circleProgressBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                //TODO Launch the details activity.
+                return true;
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_search:
+                //TODO Search
+                return true;
+            case R.id.menu_settings:
+                //TODO Settings
+                return true;
+            case R.id.menu_time_select:
+                //TODO Time select
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
